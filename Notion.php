bb27599e405ea3040bd944f7c9010e9ff574b8af
Shahmi Saidi <shahmi@technosparks.net>
2021-07-31 09:10:08 +0800

    introduce payload transmitter/receiver
    also refactor

--- Notion.php
+++ Notion.php
@@ -1,24 +1,34 @@
 <?php
 namespace Notion;
 class Notion {
-    var $token = null;
-    var $current_database = null;
+    private $apiKey = null; // holds the API key
+    var $current_database = null; // stores the ID of database if set
     const api = "https://api.notion.com/v1/";
 
-    function __construct($token = null, $current_database = null) {
+    function __construct($apiKey = null, $current_database = null) {
         // a token is required
-        if(is_null($token) || empty($token) || !isset($token)) { throw new Exception('A token is required'); return; }
-        $this->token = $token;
+        if(empty($apiKey)) { throw new Exception('A token is required'); return; }
+        $this->token = $apiKey;
         $this->database = $database;
     }
 
     function get_databases() {
-        // tbd
+        $endpoint = "databases";
     }
 
     function get_rows($id = null) {
+        $endpoint = "databases";
         // current_database must be set
-        if(is_null($current_database) || empty($current_database) || !isset($current_database)) { throw new Exception('current_database is not set'); return; }
+        if(empty($id) && empty($current_database)) { throw new Exception('database id needed'); return; }
+    }
 
+    function http_request() {
+        $options = [ 
+            'http' => [
+                "method" => "GET",
+                "header" => "Authorization: Bearer ".$apiKey."\r\n"."Notion-Version: 2021-05-13\r\n"
+        ]];
+        $context=stream_context_create($options);
+        $data=file_get_contents('http://www.someservice.com/api/fetch?key=1234567890',false,$context);
     }
 }
